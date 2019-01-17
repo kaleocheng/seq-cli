403d13724e0b0856b01ebd638678e0745691e829
Kaleo <kaleocheng@gmail.com>
2019-01-17 12:49:39 +0800

    Refactor

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const puppeteer = require('puppeteer')
 const Mustache = require('mustache')
+const path = require('path')
 const fs = require('fs');
 
 const main = (async () => {
+    const index = path.join(__dirname, 'index.html')
     const seq = fs.readFileSync('test.seq', 'utf8')
-    const template = fs.readFileSync('tmpl.mustache', 'utf8')
+    const template = fs.readFileSync('index.mustache', 'utf8')
     let output = Mustache.render(template, {
         seq: seq
     })
+    fs.writeFileSync(index, output)
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setViewport({
-        width: 800,
-        height: 800,
+        width: 2000,
+        height: 2000,
         deviceScaleFactor: 2
     });
-    //await page.goto('file:///Users/kaleocheng/Dropbox/Archives/2018/vim-sequence-diagram/plugin/tmpl.html')
-    await page.setContent(output)
+    await page.goto(`file://${index}`)
     const diagram = await page.$('svg')
     await diagram.screenshot({
-        path: 'diagram.png',
+        path: 'diagram.png'
     })
     await browser.close()
 })
