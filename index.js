24ae2f5f6f0557249e974e28afb3eb527ecb407b
Kaleo <kaleocheng@gmail.com>
2019-01-25 17:39:15 +0800

    Fix issue for backtick `

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ commander
     const seq = fs.readFileSync(commander.input, 'utf8')
     const template = fs.readFileSync(path.join(__dirname, 'index.mustache'), 'utf8')
     let output = Mustache.render(template, {
-        seq: seq
+        seq: seq.replace(/`/g, '\\`')
     })
     fs.writeFileSync(index, output)
     const config = {}
